783f44dee7c711751c5fe1e9f4f0993096536bc7
Mudit Pathak <mudit.pathak@coforge.com>
2023-11-23 13:20:37 +0530

    Update health_check

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -32,15 +32,15 @@ resource "aws_lb_target_group" "front" {
   }
 
   health_check {
-    enabled             = true
-    healthy_threshold   = lookup ( var.target_group , "healthy_threshold")
-    interval            = lookup ( var.target_group , "interval") 
+    # enabled             = true
+    # healthy_threshold   = lookup ( var.target_group , "healthy_threshold")
+    # interval            = lookup ( var.target_group , "interval") 
     # matcher             = lookup ( var.target_group , "matcher")
-    path                = lookup ( var.target_group , "path")
-    port                = lookup ( var.target_group , "port")
+    # path                = lookup ( var.target_group   , "path")
+    # port                = lookup ( var.target_group , "port")
     protocol            = lookup ( var.target_group , "protocol")
-    timeout             = lookup ( var.target_group , "timeout")
-    unhealthy_threshold = lookup ( var.target_group , "unhealthy_threshold")
+    # timeout             = lookup ( var.target_group , "timeout")
+    # unhealthy_threshold = lookup ( var.target_group , "unhealthy_threshold")
     
   }
 }
