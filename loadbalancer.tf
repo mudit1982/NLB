d9aea169da30e6d7231cbc509906386f33e686e3
Mudit Pathak <mudit.pathak@coforge.com>
2023-12-01 15:37:06 +0530

    Add policy

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -148,22 +148,22 @@ resource "aws_lb" "network-lb" {
 
 # }
 
-resource "aws_load_balancer_policy" "wu-tang-ssl-tls-1-1" {
-  load_balancer_name = aws_lb.network-lb.name
-  policy_name        = "wu-tang-ssl"
-  policy_type_name   = "SSLNegotiationPolicyType"
+# resource "aws_load_balancer_policy" "wu-tang-ssl-tls-1-1" {
+#   load_balancer_name = aws_lb.network-lb.name
+#   policy_name        = "wu-tang-ssl"
+#   policy_type_name   = "SSLNegotiationPolicyType"
 
-  policy_attribute {
-    name  = "Reference-Security-Policy"
-    value = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
-  }
-}
+#   policy_attribute {
+#     name  = "Reference-Security-Policy"
+#     value = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
+#   }
+# }
 
 resource "aws_load_balancer_listener_policy" "wu-tang-listener-policies-443" {
   load_balancer_name = aws_lb.network-lb.name
   load_balancer_port = 443
 
   policy_names = [
-    "wu-tang-ssl",
+    "ELBSecurityPolicy-TLS-1-2-Ext-2018-06",
   ]
 }
